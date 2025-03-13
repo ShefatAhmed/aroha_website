884c959c0d35b9d99932d0ddc2d427ebf9fbdff4
unknown <shefata69@gmail.com>
2025-03-13 15:42:48 +0600

    add footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,9 +1,94 @@
-import React from 'react';
+import { FaInstagram, FaFacebookF, FaPinterestP, FaYoutube } from "react-icons/fa";
+import { RxCross2 } from "react-icons/rx";
 
 const Footer = () => {
     return (
-        <div>
+        <footer className="bg-[#37494F] text-white pt-10 pb-16">
+        <div className="container mx-auto px-28">
+          <div className="grid grid-cols-1 md:grid-cols-4 gap-6 text-center md:text-left">
+            {/* Shop Section */}
+            <div>
+              <h3 className="font-semibold mb-2">Shop</h3>
+              <ul className="text-sm space-y-1">
+                <li>
+                  <a href="#" className="hover:underline">
+                    Categories
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Discounted products
+                  </a>
+                </li>
+              </ul>
+            </div>
+  
+            {/* Sell Section */}
+            <div>
+              <h3 className="font-semibold mb-2">Sell</h3>
+              <ul className="text-sm space-y-1">
+                <li>
+                  <a href="#" className="hover:underline">
+                    Sell on Etsy
+                  </a>
+                </li>
+              </ul>
+            </div>
+  
+            {/* About Section */}
+            <div>
+              <h3 className="font-semibold mb-2">About</h3>
+              <ul className="text-sm space-y-1">
+                <li>
+                  <a href="#" className="hover:underline">
+                    About Aroha
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Privacy Policy
+                  </a>
+                </li>
+                <li>
+                  <a href="#" className="hover:underline">
+                    Terms & Conditions
+                  </a>
+                </li>
+              </ul>
+            </div>
+  
+            {/* Help & Social Icons */}
+            <div>
+              <h3 className="font-semibold mb-2">Help</h3>
+              <ul className="text-sm space-y-1">
+                <li>
+                  <a href="#" className="hover:underline">
+                    Help Center
+                  </a>
+                </li>
+              </ul>
+              {/* Social Icons */}
+              <div className="flex justify-center md:justify-start space-x-4 mt-4 text-xl">
+                <a href="#" className="hover:text-gray-300">
+                  <FaInstagram />
+                </a>
+                <a href="#" className="hover:text-gray-300">
+                  <FaFacebookF />
+                </a>
+                <a href="#" className="hover:text-gray-300">
+                  <FaPinterestP />
+                </a>
+                <a href="#" className="hover:text-gray-300">
+                  <RxCross2 />
+                </a>
+                <a href="#" className="hover:text-gray-300">
+                  <FaYoutube />
+                </a>
+              </div>
+            </div>
+          </div>
         </div>
+      </footer>
     );
 };
 
